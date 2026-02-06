120749535b25c6c30eb0d5dbb415b786d1983913
RicardoAALL <77858159+RicardoAALL@users.noreply.github.com>
2026-02-06 14:06:05 -0700

    refactor: wrapped code in main function

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,28 @@
 import { Command } from "commander";
 import { handleRunCommand } from "./commands.js";
 
-const program = new Command();
+const main = async () => {
+  const program = new Command();
 
-program
-  .name("web-quote-emailer")
-  .description("CLI to get unsent web quotes for agents and email them")
-  .version("0.1.0");
+  program
+    .name("web-quote-emailer")
+    .description("CLI to get unsent web quotes for agents and email them")
+    .version("0.1.0");
 
-program
-  .command("run")
-  .description("execute the web quote emailer process")
-  .argument(
-    "<type>",
-    "type of email to send: abandoned, completed, scheduled-callback",
-  )
-  .option(
-    "--older-than [minutes]",
-    "get web quotes older than specified minutes - 30 is the default",
-    "30",
-  )
-  .action(handleRunCommand);
+  program
+    .command("run")
+    .description("execute the web quote emailer process")
+    .argument(
+      "<type>",
+      "type of email to send: abandoned, completed, scheduled-callback",
+    )
+    .option(
+      "--older-than [minutes]",
+      "get web quotes older than specified minutes - 30 is the default",
+      "30",
+    )
+    .action(handleRunCommand);
 
-await program.parseAsync(process.argv);
+  await program.parseAsync(process.argv);
+};
+main();
